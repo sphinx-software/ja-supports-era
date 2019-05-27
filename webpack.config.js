6f530c7d68501e85bc8a6e807df0d9a8a525287e
rikky <son.levuthai@gmail.com>
2019-05-27 11:34:53 +0700

    excluding node_modules for better performance

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,7 @@ module.exports = {
             {
                 test: /\.js$/,
                 loader: 'babel-loader',
+                exclude: /node_modules/,
                 query: {
                     presets: ['@babel/env']
                 }
